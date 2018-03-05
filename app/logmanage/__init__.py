44b51da38cf5cc58e405e35fe7dd825ed524c638
shenfeng <shf1024@vip.qq.com>
2018-03-05 17:41:38 +0800

    创建项目目录

---
+++ app/logmanage/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/venv/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/5 16:42
+# @Author  : ShenFeng
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/venv/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/5 16:36
+# @Author  : ShenFeng
+# @Site    : 
+# @File    : config.py
+# @Software: PyCharm
+
+
